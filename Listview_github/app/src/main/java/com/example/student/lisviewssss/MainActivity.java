34dd40410b0c88df71d920760e4bb5b377445533
Your Name <hoang6428@gmail.com>
2019-02-20 10:56:37 +0700

    da sua lan 3

--- Listview_github/app/src/main/java/com/example/student/lisviewssss/MainActivity.java
+++ Listview_github/app/src/main/java/com/example/student/lisviewssss/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.student.lisviewssss;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lvwds;
     ArrayList<String> arrds;
     ArrayAdapter addds;
+    int vitri=1;
 
     void anhxa(){
         lvwds=findViewById(R.id.lvw_ds);
@@ -32,11 +35,24 @@ public class MainActivity extends AppCompatActivity {
         arrds.add("Nhan");
         addds=new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,arrds);
         lvwds.setAdapter(addds);
+        lvwds.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                edtten.setText(arrds.get(i));
+                vitri=i;
+            }
+        });
     }
 
     public void xlthem(View view) {
         String ten=edtten.getText().toString();
         arrds.add(ten);
+        addds.notifyDataSetChanged();
+    }
+
+    public void xlsua(View view) {
+        arrds.set(vitri,edtten.getText().toString());
+        Toast.makeText(MainActivity.this,"sua thanh cong",Toast.LENGTH_LONG).show();
         addds.notifyDataSetChanged();
     }
 }
